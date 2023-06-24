8ea6eaf0c0db9b2576ca3c9b85bdb0ff76da0f57
Adrian Lopez <adrianlopez@Adrians-MacBook-Air.local>
2023-06-23 20:45:18 -0500

    added mian.cpp header

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-/*
- * your header here
- * 
- */
+/*******************
+*coding assignment 05: int stack
+*Name: Adrian Lopez
+*Date: 06/19/23
+*Status:in progress
+*Language: C++
+*File: main.cpp
+**********************/
 
 #include "main.h"
 
