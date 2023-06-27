c25779315a7af7d13a9e5a7af3aba685f7f5d904
Adrian Lopez <adrianlopez@Adrians-Air.attlocal.net>
2023-06-26 22:14:30 -0500

    added consent ARC_LENGTH

--- main.h
+++ main.h
@@ -7,12 +7,13 @@
 *File: main.h
 **********************/
 #ifndef STACKS_MAIN_H
+#define ARC_LENGTH 2
 #define STACKS_MAIN_H
 
 // you probably need all these,
 // but if you don't get rid of what you don't need
 #include <stdlib.h>   /* srand, rand */
-#include <time.h>     /* time */
+#include <time.h>   /* time */   
 #include <iostream>   /* cout, endl */
 #include <string>     /* string */
 #include "functions.h"
