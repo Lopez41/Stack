d351c31016fdfe47aee0692889da037a503af3cb
Adrian Lopez <adrianlopez@Adrians-Air.attlocal.net>
2023-06-26 22:16:23 -0500

    Removed std and varibles in prototypes

--- stack.h
+++ stack.h
@@ -8,11 +8,12 @@
 *******************/
 #ifndef STACK_H
 #define STACK_H
+#define DEFAULT_MAX 10
+#define MIN_STACK 2
 #include "data.h"
 #include <iostream>
 #include <stdexcept>
-using std::cout;
-using std::endl;
+
 /*
  * there are no other includes here because they are not needed.
  * if you do need other things included, they would go here
@@ -25,15 +26,15 @@ public:
      * write all your public method prototypes here
      * this includes you constructor and destructor
      */
-    Stack(int stackSize);
+    Stack(int);
     //deconstructor
     ~Stack();
     //method to pop data off the stack
-    bool pop(Data* poppedData);
+    bool pop(Data*);
     //method to peek at the top of the stack
-    bool peek(Data* topData);
+    bool peek(Data*);
     //method to push data onto stack
-    bool push(int id,string*);
+    bool push(int,string*);
     //method to check is stack is empty
     bool isEmpty();
    // get the size of stack
